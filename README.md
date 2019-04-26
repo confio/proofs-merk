40f000272f2a2b9a3b1c99a9666836202f40743e
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-04-26 19:17:33 +0200

    Initial demo

---
+++ README.md
@@ -0,0 +1,14 @@
+# Proofs merk
+
+Creating generic [@confio/proofs](https://github.com/confio/proofs) from "merk" output.
+
+## Demo
+
+This is just demo code now to understand the format.
+
+**Required node 10+** (async support)
+
+```shell
+yarn install
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+const merk = require('merk');
+const levelup = require('levelup');
+const memdown = require('memdown');
+
+async function demoTree() {
+    const db = levelup(memdown());
+    let state = await merk(db);
+
+    // TODO: add more data
+    state.foo = 'bar';
+    state.food = 'yummy';
+    state.bath = {tub: 'full', room: 'small'};
+    state.baz = { x: 123, y: { z: 456 } };
+
+    await merk.commit(state)
+
+    let rootHash = merk.hash(state)
+    console.log(`root: ${rootHash}`)
+
+    // create a JSON Merkle proof of the queried path
+    let proof = await merk.proof(state, 'baz.y')
+    console.log("")
+    console.log(proof)
+
+    proof = await merk.proof(state, 'food')
+    console.log("")
+    console.log(proof)
+
+    proof = await merk.proof(state, 'baz')
+    console.log("")
+    console.log(proof)
+} 
+
+demoTree().then(() => console.log("done"));
