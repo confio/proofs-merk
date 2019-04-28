c176b1c3d4261a4af33ed5512aae48630beb0f5a
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-04-28 15:24:54 +0200

    Calculate root from proof... works for leaf, fails for one step

--- index.spec.js
+++ index.spec.js
@@ -1,33 +1,8 @@
-const { extractOps, getKeyValue } = require("./index");
+const { calculateExistenceRoot } = require("@confio/proofs"); 
+const { Encoding } = require("@iov/encoding");
+
+const { convertProof, extractOps, getKeyValue } = require("./index");
  
-describe("extractOps", () => {
-    it("can convert a leaf node", () => {
-        const input = { left: null, right: null, key: '.', value: '{"dead":"beef"}' };
-        const std = extractOps(input);
-        // one leaf and one inner
-        expect(std.length).toEqual(2); 
-
-        // const rootHash = "986e31a92d538f98da9378a4c6e49ad8c03b9470";
-    });
-
-    it("can convert a sample proof", () => {
-        const input = { 
-            left: 'F4j9DF3/N4rBqoX4JYyW9AdvDAo=',
-            right: { 
-                left: null, 
-                right: null, 
-                key: '.baz.y', 
-                value: '{"z":456}' 
-            },
-            key: '.baz',
-            value: '{"x":123}'
-        };
-        const std = extractOps(input);
-        // one leaf and two inner
-        expect(std.length).toEqual(3); 
-    });
-});
-
 describe("getKeyValue", () => {
     it("works with leaf node", () => {
         const input = { left: null, right: null, key: '.', value: '{"dead":"beef"}' };
@@ -51,5 +26,66 @@ describe("getKeyValue", () => {
         const {key, value} = getKeyValue(input);
         expect(key).toEqual('.baz.y');
         expect(value).toEqual('{"z":456}');
+    });
+});
+
+describe("extractOps", () => {
+    it("can convert a leaf node", () => {
+        const input = { left: null, right: null, key: '.', value: '{"dead":"beef"}' };
+        const std = extractOps(input);
+        // one leaf and one inner
+        expect(std.length).toEqual(2); 
+    });
+
+    it("can convert a sample proof", () => {
+        const input = { 
+            left: 'F4j9DF3/N4rBqoX4JYyW9AdvDAo=',
+            right: { 
+                left: null, 
+                right: null, 
+                key: '.baz.y', 
+                value: '{"z":456}' 
+            },
+            key: '.baz',
+            value: '{"x":123}'
+        };
+        const std = extractOps(input);
+        // one leaf and two inner
+        expect(std.length).toEqual(3); 
+    });
+});
+
+
+describe("convertProot", () => {
+    it("can convert a leaf node", () => {
+        const input = { left: null, right: null, key: '.', value: '{"dead":"beef"}' };
+        const std = convertProof(input);
+        // one leaf and one inner
+        expect(std.steps.length).toEqual(2); 
+        expect(std.key).toEqual(Encoding.toAscii('.'));
+
+        const root = calculateExistenceRoot(std);
+        expect(Encoding.toHex(root)).toEqual("986e31a92d538f98da9378a4c6e49ad8c03b9470");
+    });
+
+    it("can convert a sample proof", () => {
+        const input = { 
+            left: 'F4j9DF3/N4rBqoX4JYyW9AdvDAo=',
+            right: { 
+                left: null, 
+                right: null, 
+                key: '.baz.y', 
+                value: '{"z":456}' 
+            },
+            key: '.baz',
+            value: '{"x":123}'
+        };
+        const std = convertProof(input);
+
+        expect(std.steps.length).toEqual(3); 
+        expect(std.key).toEqual(Encoding.toAscii('.baz.y'));
+
+        const root = calculateExistenceRoot(std);
+        expect(Encoding.toHex(root)).toEqual("a5a3a7255ef65deea2ba17b22b3b5b1521628a22");
     });
 });
